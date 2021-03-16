d9e2bcecf5056231e9b4bffd2408fc70f5d0c678
xiangjingjing1 <1012985323@qq.com>
2021-03-16 14:56:35 +0100

    Initial Commit

---
+++ FDJ/FDJApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDJApp.swift
+//  FDJ
+//
+//  Created by Jingjing XIANG on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FDJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
