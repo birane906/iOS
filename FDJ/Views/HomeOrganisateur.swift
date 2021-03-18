5cf6bc5c5a1ea16cfaf3b1a345f7bf85f5bad822
xiangjingjing1 <1012985323@qq.com>
2021-03-18 22:27:29 +0100

    Add login view

---
+++ FDJ/Views/HomeOrganisateur.swift
@@ -0,0 +1,20 @@
+//
+//  HomeOrganisateur.swift
+//  FDJ
+//
+//  Created by Jingjing XIANG on 18/03/2021.
+//
+
+import SwiftUI
+
+struct HomeOrganisateur: View {
+    var body: some View {
+        Text("Home Organisateur")
+    }
+}
+
+struct HomeOrganisateur_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeOrganisateur()
+    }
+}
---
+++ FDJ/Views/HomeVisiteur.swift
@@ -0,0 +1,20 @@
+//
+//  HomeVisiteur.swift
+//  FDJ
+//
+//  Created by Jingjing XIANG on 18/03/2021.
+//
+
+import SwiftUI
+
+struct HomeVisiteur: View {
+    var body: some View {
+        Text("Home Visateur")
+    }
+}
+
+struct HomeVisiteur_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeVisiteur()
+    }
+}
